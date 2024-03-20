663c505bf42d440a3014edab352bb6117e86cb4b
Iskandar Sholeh <iskandar.sholeh22@gmail.com>
2024-03-20 15:32:16 +0700

    done

--- application/views/home.php
+++ application/views/home.php
@@ -7,39 +7,53 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 pt-5">
-                <div class="card">
-                    <img src="<?= base_url().'assets/images/Menu.png'; ?>" class="card-img-top" alt="Image 1">
-                </div>
+                <a href="<?= base_url().'tentang' ?>">
+                    <div class="card">
+                        <img src="<?= base_url().'assets/images/Menu.png'; ?>" class="card-img-top" alt="Image 1">
+                    </div>
+                </a>   
             </div>
             <div class="col-md-4 pt-5">
-                <div class="card">
-                    <img  src="<?= base_url().'assets/images/Menu(1).png'; ?>"  class="card-img-top" alt="Image 2">
-                </div>
+                <a href="<?= base_url().'petunjuk' ?>">
+                    <div class="card">
+                        <img  src="<?= base_url().'assets/images/Menu(1).png'; ?>"  class="card-img-top" alt="Image 2">
+                    </div>
+                </a>  
             </div>
             <div class="col-md-4 pt-5">
-            <div class="card">
-                    <img  src="<?= base_url().'assets/images/Menu(2).png'; ?>"  class="card-img-top" alt="Image 2">
-                </div>
+                <a href="<?= base_url().'materi' ?>">
+                    <div class="card">
+                            <img  src="<?= base_url().'assets/images/Menu(2).png'; ?>"  class="card-img-top" alt="Image 2">
+                        </div>
+                    </div>
+                </a>
+            <div class="col-md-4 pt-5">
+                <a href="<?= base_url().'video' ?>">
+                    <div class="card">
+                        <img  src="<?= base_url().'assets/images/Menu(3).png'; ?>"  class="card-img-top" alt="Image 2">
+                    </div>
+                </a>
             </div>
             <div class="col-md-4 pt-5">
-               <div class="card">
-                    <img  src="<?= base_url().'assets/images/Menu(3).png'; ?>"  class="card-img-top" alt="Image 2">
-                </div>
+                <a href="<?= base_url().'kuis' ?>">
+                    <div class="card">
+                        <img  src="<?= base_url().'assets/images/Menu(4).png'; ?>"  class="card-img-top" alt="Image 2">
+                    </div>
+                </a>
             </div>
             <div class="col-md-4 pt-5">
-               <div class="card">
-                    <img  src="<?= base_url().'assets/images/Menu(4).png'; ?>"  class="card-img-top" alt="Image 2">
-                </div>
-            </div>
-            <div class="col-md-4 pt-5">
-                <div class="card">
-                    <img  src="<?= base_url().'assets/images/Menu(5).png'; ?>"  class="card-img-top" alt="Image 2">
-                </div>
+                <a href="<?= base_url().'game' ?>">
+                    <div class="card">
+                        <img  src="<?= base_url().'assets/images/Menu(5).png'; ?>"  class="card-img-top" alt="Image 2">
+                    </div>
+                </a>
             </div>
             <div class="col-md-4 pt-5 pb-5 offset-md-4">
-                <div class="card">
-                    <img  src="<?= base_url().'assets/images/Menu(6).png'; ?>"  class="card-img-top" alt="Image 2">
-                </div>
+                <a href="<?= base_url().'profil' ?>">
+                    <div class="card">
+                        <img  src="<?= base_url().'assets/images/Menu(6).png'; ?>"  class="card-img-top" alt="Image 2">
+                    </div>
+                </a>    
             </div>
         </div>
     </div>
