a1b51e35b3e2dc342bdd193892293f1f8c17363d
Iskandar Sholeh <iskandar.sholeh22@gmail.com>
2024-03-20 15:25:40 +0700

    done

--- application/views/home.php
+++ application/views/home.php
@@ -3,4 +3,46 @@
         <h1><b>SELAMAT DATANG !</b></h1>
         <p>Yuk, pilih menu dibawah ini !</p>
     </center>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 pt-5">
+                <div class="card">
+                    <img src="<?= base_url().'assets/images/Menu.png'; ?>" class="card-img-top" alt="Image 1">
+                </div>
+            </div>
+            <div class="col-md-4 pt-5">
+                <div class="card">
+                    <img  src="<?= base_url().'assets/images/Menu(1).png'; ?>"  class="card-img-top" alt="Image 2">
+                </div>
+            </div>
+            <div class="col-md-4 pt-5">
+            <div class="card">
+                    <img  src="<?= base_url().'assets/images/Menu(2).png'; ?>"  class="card-img-top" alt="Image 2">
+                </div>
+            </div>
+            <div class="col-md-4 pt-5">
+               <div class="card">
+                    <img  src="<?= base_url().'assets/images/Menu(3).png'; ?>"  class="card-img-top" alt="Image 2">
+                </div>
+            </div>
+            <div class="col-md-4 pt-5">
+               <div class="card">
+                    <img  src="<?= base_url().'assets/images/Menu(4).png'; ?>"  class="card-img-top" alt="Image 2">
+                </div>
+            </div>
+            <div class="col-md-4 pt-5">
+                <div class="card">
+                    <img  src="<?= base_url().'assets/images/Menu(5).png'; ?>"  class="card-img-top" alt="Image 2">
+                </div>
+            </div>
+            <div class="col-md-4 pt-5 pb-5 offset-md-4">
+                <div class="card">
+                    <img  src="<?= base_url().'assets/images/Menu(6).png'; ?>"  class="card-img-top" alt="Image 2">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </main>
