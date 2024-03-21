6793f5215fc4d145a7fe23fe7568f635eebdf4c9
Iskandar Sholeh <iskandar.sholeh22@gmail.com>
2024-03-22 06:14:48 +0700

    done

--- application/views/game.php
+++ application/views/game.php
@@ -1,7 +1,23 @@
+<style>
+    /* Style untuk image */
+    .corner-image {
+        width: 100px; /* ukuran gambar */
+        position: fixed; /* posisi tetap */
+        bottom: 0; /* jarak dari bawah */
+        right: 0; /* jarak dari kanan */
+        margin: 20px; /* margin untuk memberi ruang */
+    }
+</style>
+
 <main class="container mt-5">
-    <center>
-        <h1><b>INI GAME</b></h1>
-       
-    </center>
     
+<a class="btn btn-outline-secondary px-3" href="<?php echo base_url(); ?>home" style="color: white; background-color: #6c757d; border-color: #6c757d;">Back</a>
+
+
+    <center>
+        <h1><b> GAME</b></h1>
+
+        <iframe src="https://wordsearchlabs.com/embed/532172" frameborder="0" width="400" height="500"></iframe>
+    </center>
+    <img src="<?php echo base_url(); ?>" alt="Corner Image" class="corner-image">
 </main>
--- application/views/kuis.php
+++ application/views/kuis.php
@@ -1,7 +1,8 @@
 <main class="container mt-5">
+<a class="btn btn-outline-secondary px-3" href="<?php echo base_url(); ?>home" style="color: white; background-color: #6c757d; border-color: #6c757d;">Back</a>
     <center>
-        <h1><b>INI KUIS</b></h1>
-       
+        <h1><b> KUIS</b></h1>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfplowWm-1kLg4m_0NmzUBLe3js_LaBtrY8RDvN5ixmWWQB9w/viewform?embedded=true" width="640" height="3417" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
     </center>
     
 </main>
